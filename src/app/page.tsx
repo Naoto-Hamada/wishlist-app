41dfcf8cf488158465da4f3b4bb347394811ed71
Naoto.h <mr.myself710@gamil.com>
2024-10-20 21:13:28 +0900

    変更：page.tsxに、home.tsxが表示されるように修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
+import { HomeComponent } from "@/components/home";
 
 export default function Home() {
   return (
     <div>
-      <header className="bg-gradient-to-r from-primary-start to-primary-end text-text-inverted p-4">
-        <h1 className="text-2xl font-bold">ダッシュボード</h1>
-      </header>
-      <main className="p-4">
-        <section className="bg-white rounded-lg shadow-md p-4 mb-4">
-          <h2 className="text-xl font-semibold text-text-primary mb-2">直近のやりたいタスク</h2>
-          {/* タスクリストコンポーネント */}
-        </section>
-        <section className="bg-white rounded-lg shadow-md p-4">
-          <h2 className="text-xl font-semibold text-text-primary mb-2">達成状況</h2>
-          {/* チャートコンポーネント */}
-        </section>
-      </main>
-      <nav className="bg-background-secondary fixed bottom-0 w-full p-4">
-        {/* ナビゲーションアイテム */}
-      </nav>
+      <HomeComponent />
     </div>
   );
 }
